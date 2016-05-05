5c53d4d945218fcdb8887a590aa6bc3fa1a1c310
jasndr <jdelosr@VED-HP>
2016-05-04 17:02:49 -1000

    Added Pictures to the main page of the site.

--- WebApplication2/JavaScript.js
+++ WebApplication2/JavaScript.js
@@ -1,1 +1,40 @@
-﻿
+﻿img2 = new Image()
+
+seconds = "5";
+
+function imgOne(){
+    setTimeout("imgTwo()", seconds*1000);
+}
+
+function imgTwo() {
+    document.myimg.src="BSB_LOBBY_FEB2015-img2.jpg"; //Place Image 2 Here
+    setTimeout("imgThree()", seconds*1000);
+}
+
+function imgThree(){
+    document.myimg.src="sym_2015_core-img3.jpg"; //Place Image 3 Here
+    setTimeout("imgFour()", seconds*1000);
+}
+
+function imgFour() {
+    document.myimg.src="JABSOM_FEB2015-img4.jpg"; //Place Image 4 Here
+    setTimeout("imgFive()", seconds * 1000);
+}
+
+function imgFive() {
+    document.myimg.src = "books-img5.jpg"; //Place Image 5 Here
+    setTimeout("imgSix()", seconds * 1000);
+}
+
+function imgSix() {
+    document.myimg.src = "kakaako-edit3-img6.jpg"; //Place Image 6 Here
+    setTimeout("imgSeven()", seconds * 1000);
+}
+
+function imgSeven() {
+    document.myimg.src = "BSB_FEB2015-img7.jpg"; //Place Image 7 Here
+    setTimeout("imgOne()", seconds * 1000);
+}
+
+
+

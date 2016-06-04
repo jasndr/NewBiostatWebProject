eec7185175a02472287eb86463a0239ec268d911
jasndr <jdelosr@hawaii.edu>
2016-06-03 17:01:08 -1000

    Created the "Expertise" and "Collaborations" pages. Tried to mimic/imitate Cancer Center style.  Also halfway through "Resources Pages".

--- WebApplication2/JavaScript.js
+++ WebApplication2/JavaScript.js
@@ -1,1 +1,23 @@
 ﻿
+//Removed the idea of clickable/hoverable drop-down menu area (could be implemented in the future).
+/**$(document).ready(function () {
+
+    var listOfTitles = ["#rdTitle", "#daTitle", "#redTitle", "#teTitle", "#fcTitle", "#fgTitle"];
+    var listOfContent = ["#rdContent", "#daContent", "#redContent", "#teContent", "#fcContent", "#fgContent"];
+
+    $("#stContent").addClass("hidden");
+    
+    $("#stTitle").click(function () {
+
+        var stContent = $("#stContent");
+
+        if (stContent.hasClass("hidden")) {
+            $(stContent).removeClass("hidden").addClass("visible");
+        } else {
+            $(stContent).removeClass("visible").addClass("hidden");
+        }
+
+
+    });
+
+});*/

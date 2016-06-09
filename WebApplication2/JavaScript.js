b37a6229fa1886c0bdd764b64cf13a7985cd005e
jasndr <jdelosr@hawaii.edu>
2016-06-08 17:05:43 -1000

    Implementing search bar function.

--- WebApplication2/JavaScript.js
+++ WebApplication2/JavaScript.js
@@ -1,5 +1,39 @@
 ﻿
 
+/**
+function doSearch(s) {
+    var regExp1 = /\bfield\b/;
+    var regExp2 = /[(,),<,>,\,]]/;
+    var str = s.value;
+
+    if (str == "") {
+        s.focus();
+    } else {
+        if (typeof regExp1.source != 'undefined') //supports regular expression testing
+            if (regExp1.test(str) || regExp2.test(str)) {
+                var alrt = "Please note that you can not include: ";
+                alrt += "\n\nThe reserved word 'field'\nthe characters [, ], (, ), < or >";
+                alrt += "\n\nin your search query!\n\nIf you are confident that you know";
+                alrt += "\nwhat you are doing, that you can\nmanually produce the URL required.";
+                s.focus();
+                return alert(alrt);
+            }
+
+        openDbRelativeURL("All?SearchView&Query=" + escape(str) + "&start=1&count=10");
+    }
+} 
+
+function openDbRelativeURL(url, target) {
+
+    //Check whether or not we have a target window
+    target = (target == null) ? window : target;
+
+    //Work out the path of the database
+    path = location.pathname.split('.nsf')[0] + '.nsf/';
+    target.location.href = path + url;
+
+} */
+
 //Tried to Implement Google Maps but didn't work. Will use embed feature instead.
 /**
 //Google Maps Feature

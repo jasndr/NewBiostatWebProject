28f85fc550b7fb892e0b03f3246e8d99aa3b4741
jasndr <jdelosr@hawaii.edu>
2016-07-08 17:27:28 -1000

    Found a solution to the div scrollbar issue, restructured the "Education" section to conform to the rest of the website, completed "Research" edits, and also attempted to fixed the JavaScript search.

--- WebApplication2/JavaScript.js
+++ WebApplication2/JavaScript.js
@@ -26,42 +26,6 @@ $(document).ready(function () {
 });
 
 
-
-
-/**
-function doSearch(s) {
-    var regExp1 = /\bfield\b/;
-    var regExp2 = /[(,),<,>,\,]]/;
-    var str = s.value;
-
-    if (str == "") {
-        s.focus();
-    } else {
-        if (typeof regExp1.source != 'undefined') //supports regular expression testing
-            if (regExp1.test(str) || regExp2.test(str)) {
-                var alrt = "Please note that you can not include: ";
-                alrt += "\n\nThe reserved word 'field'\nthe characters [, ], (, ), < or >";
-                alrt += "\n\nin your search query!\n\nIf you are confident that you know";
-                alrt += "\nwhat you are doing, that you can\nmanually produce the URL required.";
-                s.focus();
-                return alert(alrt);
-            }
-
-        openDbRelativeURL("All?SearchView&Query=" + escape(str) + "&start=1&count=10");
-    }
-} 
-
-function openDbRelativeURL(url, target) {
-
-    //Check whether or not we have a target window
-    target = (target == null) ? window : target;
-
-    //Work out the path of the database
-    path = location.pathname.split('.nsf')[0] + '.nsf/';
-    target.location.href = path + url;
-
-} */
-
 //Removed the idea of clickable/hoverable drop-down menu area (could be implemented in the future).
 /**$(document).ready(function () {
 
@@ -84,3 +48,39 @@ function openDbRelativeURL(url, target) {
     });
 
 });*/
+
+
+
+//function doSearch(s) {
+//    var regExp1 = /\bfield\b/;
+//    var regExp2 = /[(,),<,>,\,]]/;
+//    var str = s.value;
+
+//    if (str == "") {
+//        s.focus();
+//    } else {
+//        if (typeof regExp1.source != 'undefined') //supports regular expression testing
+//            if (regExp1.test(str) || regExp2.test(str)) {
+//                var alrt = "Please note that you can not include: ";
+//                alrt += "\n\nThe reserved word 'field'\nthe characters [, ], (, ), < or >";
+//                alrt += "\n\nin your search query!\n\nIf you are confident that you know";
+//                alrt += "\nwhat you are doing, that you can\nmanually produce the URL required.";
+//                s.focus();
+//                return alert(alrt);
+//            }
+
+//        openDbRelativeURL("All?SearchView&Query=" + escape(str) + "&start=1&count=10");
+//    }
+//} 
+
+//function openDbRelativeURL(url, target) {
+
+//    //Check whether or not we have a target window
+//    target = (target == null) ? window : target;
+
+//    //Work out the path of the database
+//    path = location.pathname.split('.nsf')[0] + '.nsf/';
+//    target.location.href = path + url;
+
+} 
+

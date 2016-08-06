decd4313dcd5ee5cfdca767e09bdae88f71cf5b2
jasndr <jdelosr@hawaii.edu>
2016-08-05 17:22:54 -1000

    Attempted minor changes to left-hand navigation bar in order to not make it overlap with footer.

--- WebApplication2/JavaScript.js
+++ WebApplication2/JavaScript.js
@@ -7,6 +7,80 @@ $('.panel-heading a').on('click', function(e){
 
 });
 
+$(document).ready(function () {
+
+    var smallMenu = $('.smallMenu');
+    var minTop = $('.pageHeader').outerHeight();
+    var maxTop = $('.bottomFooter').offset().top - smallMenu.outerHeight();
+
+
+    $(document).scroll(function () {
+
+        var scrollVal = $(document).scrollTop();
+
+        smallMenu.css('top', scrollVal);
+
+        if (scrollVal < minTop) {
+            smallMenu.css('top', minTop);
+        }
+
+        if (smallMenu.offset().top > maxTop) {
+            smallMenu.css('top', maxTop);
+        }
+
+
+    });
+
+
+});
+
+//$(document).ready(function () {
+
+//    if ($('.smallMenu').length) {
+
+//        var smallMenu = $('.smallMenu');
+//        var smallMenuTop = $('.smallMenu').offset().top;
+//        var smallMenuHeight = $('.smallMenu').height();
+
+      
+  
+
+//        $(window).scroll(function () {
+            
+//            var limit = $('.smallMenu').offset().top - smallMenuHeight;
+
+//            var windowTop = $(window).scrollTop();
+
+//            if (smallMenuTop > windowTop) {
+//                smallMenu.css({ position: 'fixed'});
+//            } else {
+//                smallMenu.css({ position: 'static' });
+//            }
+            
+//            if (limit < windowTop) {
+//                var diff = limit - windowTop;
+//                smallMenu.css({ top: diff });
+//            }
+
+           
+            
+
+
+//        });
+
+//    }
+
+//    =============================================================================
+
+
+
+
+//});
+
+
+
+
+
 //jQuery 
 $(document).ready(function () {
 
@@ -106,36 +180,5 @@ $(document).ready(function () {
 
 
 
-//function doSearch(s) {
-//    var regExp1 = /\bfield\b/;
-//    var regExp2 = /[(,),<,>,\,]]/;
-//    var str = s.value;
 
-//    if (str == "") {
-//        s.focus();
-//    } else {
-//        if (typeof regExp1.source != 'undefined') //supports regular expression testing
-//            if (regExp1.test(str) || regExp2.test(str)) {
-//                var alrt = "Please note that you can not include: ";
-//                alrt += "\n\nThe reserved word 'field'\nthe characters [, ], (, ), < or >";
-//                alrt += "\n\nin your search query!\n\nIf you are confident that you know";
-//                alrt += "\nwhat you are doing, that you can\nmanually produce the URL required.";
-//                s.focus();
-//                return alert(alrt);
-//            }
-
-//        openDbRelativeURL("All?SearchView&Query=" + escape(str) + "&start=1&count=10");
-//    }
-//} 
-
-//function openDbRelativeURL(url, target) {
-
-//    //Check whether or not we have a target window
-//    target = (target == null) ? window : target;
-
-//    //Work out the path of the database
-//    path = location.pathname.split('.nsf')[0] + '.nsf/';
-//    target.location.href = path + url;
-
-//} 
 

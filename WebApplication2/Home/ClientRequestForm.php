172fa708fd5efcdd877a2d4e926b796acabecbf7
jasndr <jasndr@gmail.com>
2016-10-24 15:27:13 -1000

    Finished the send-mail feature of the ClientRequestForm that is now in "live" status.

--- WebApplication2/Home/ClientRequestForm.php
+++ WebApplication2/Home/ClientRequestForm.php
@@ -1,7 +1,9 @@
-﻿<?php 
-$your_email ='jdelosr@hawaii.edu';// <<=== update to your email address
-
+<?php 
 session_start();
+
+$your_email ='biostat@hawaii.edu';// <<=== update to your email address
+
+
 $errors = '';
 $user_message = '';
 //----------------------
@@ -41,12 +43,11 @@ if(isset($_POST['submit']))
 	$txtDueDate = $_POST['txtDueDate'];
 	$txtPreferBiostat = $_POST['txtPreferBiostat'];
 	
-	$txtCaptchaCode = $_POST['txtCaptchaCode'];
+	//$txtCaptchaCode = $_POST['txtCaptchaCode'];
 
 	
 	
 	///------------Do Validations-------------
-//(HTML ALREADY DOES FIELD VALIDATIONS)
 /**	if(empty($name)||empty($visitor_email))
 	{
 		$errors .= "\n Name and Email are required fields. ";	
@@ -56,8 +57,8 @@ if(isset($_POST['submit']))
 		$errors .= "\n Bad email value!";
 	}*/
 	
-	if(empty($txtCaptchaCode) ||
-	  strcasecmp($txtCaptchaCode, $_POST['6_letters_code']) != 0)
+	if(empty($_SESSION['6_letters_code']) ||
+	  strcasecmp($_SESSION['6_letters_code'], $_POST['6_letters_code']) != 0)
 	{
 	//Note: the captcha code is compared case insensitively.
 	//if you want case sensitive match, update the check above to
@@ -74,31 +75,36 @@ if(isset($_POST['submit']))
 		$ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
 		
 		
-		$body = "Aloha BQHS Admin,\n\n".
+		$tblStudyArea = join(", ", $_POST['tableOne']);
+		$tblServiceType = join(", ", $_POST['tableTwo']);
+		
+		
+		$body = "Aloha BQHS Admin,\n\n\n".
 		"You have received a request from $txtFirstName $txtLastName.\n".
-		"Please view their submitted information below:\n\n".
-		"First Name: $txtFirstName\n".
-		"Last Name: $txtLastName\n".
-		"Degree: $txtDegree\n".
-		"Email: $txtEmail\n".
-		"Phone: $txtPhone\n".
-		"Department: $txtDept\n".
-		"Investigator Status: $ddlPIStatus\n".
-		"Project Title: $txtProjectTitle\n".
-		"Summary of project and QHS need: $txtProjectSummary\n".
-		"Study Area(s): $tblStudyArea\n".
-		"Type(s) of support needed: $tblServiceType\n".
-		"Important deadlines: $txtDueDate\n".
-		"BQHS faculty/staff preference (if any): $txtPreferBiostat\n\n".
+		"Please view their submitted information below:\n\n\n".
+		"First Name: $txtFirstName\n\n".
+		"Last Name: $txtLastName\n\n".
+		"Degree: $txtDegree\n\n".
+		"Email: $txtEmail\n\n".
+		"Phone: $txtPhone\n\n".
+		"Department: $txtDept\n\n".
+		"Investigator Status: $ddlPIStatus\n\n".
+		"Project Title: $txtProjectTitle\n\n".
+		"Summary of project and QHS need: $txtProjectSummary\n\n".
+		"Study Area(s): $tblStudyArea\n\n".
+		"Type(s) of support needed: $tblServiceType\n\n".
+		"Important deadlines: $txtDueDate\n\n".
+		"BQHS faculty/staff preference (if any): $txtPreferBiostat\n\n\n".
+		"To reply to the sender, please click 'reply' below.\n\n".
 		"Mahalo,\n".
 		"BQHS\n";
 
 		
 		
-		$headers = "From: $from \r\n";
-		$headers .= "Reply-To: $txtEmail \r\n";
+		$headers = "From: $your_email \r\n";
+		$headers = "Reply-To: $txtEmail \r\n";
 		
-		mail($to, $subject, $body,$headers);
+		mail($to, $subject, $body, $headers);
 		
 		header('Location: ../NiceSimpleContactForm/contactthanks.html');
 	}
@@ -172,7 +178,16 @@ function IsInjected($str)
 
 <body>
 
-    <form method="post" action="./ClientRequestForm.html" id="requestForm" class="form-horizontal">
+<?php
+if(!empty($errors)){
+echo "<p class='err'>".nl2br($errors)."</p>";
+}
+?>
+<div id='contact_form_errorloc' class='err'></div>
+<form method="POST" name="contact_form" 
+	action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" class="form-horizontal"> 
+
+    <!--<form method="post" action="./ClientRequestForm.php" id="requestForm" class="form-horizontal">-->
 
         <!-- Container for the entire class-->
         <div id="mainarea" class="container">
@@ -281,13 +296,13 @@ function IsInjected($str)
                             <tr>
                                 <td>
                                     <label>
-                                        <input name="ctl06" type="checkbox" value="1" />
+                                        <input name="tableOne[]" type="checkbox" value="Aging & Chronic Disease Prevention/Management" />
                                         Aging & Chronic Disease Prevention/Management
                                     </label>
                                 </td>
                                 <td>
                                     <label>
-                                        <input name="ctl08" type="checkbox" value="4" />
+                                        <input name="tableOne[]" type="checkbox" value="Nutrition & Metabolic Health" />
                                         Nutrition & Metabolic Health
                                     </label>
                                 </td>
@@ -295,13 +310,13 @@ function IsInjected($str)
                             <tr>
                                 <td>
                                     <label>
-                                        <input name="ctl11" type="checkbox" value="2" />
+                                        <input name="tableOne[]" type="checkbox" value="Cardiovascular Health" />
                                         Cardiovascular Health
                                     </label>
                                 </td>
                                 <td>
                                     <label>
-                                        <input name="ctl13" type="checkbox" value="5" />
+                                        <input name="tableOne[]" type="checkbox" value="Growth, Development, & Reproductive Health" />
                                         Growth, Development, & Reproductive Health
                                     </label>
                                 </td>
@@ -309,13 +324,13 @@ function IsInjected($str)
                             <tr>
                                 <td>
                                     <label>
-                                        <input name="ctl16" type="checkbox" value="3" />
+                                        <input name="tableOne[]" type="checkbox" value="Cancer Prevention & Health Recovery" />
                                         Cancer Prevention & Health Recovery
                                     </label>
                                 </td>
                                 <td>
                                     <label>
-                                        <input name="ctl18" type="checkbox" value="6" />
+                                        <input name="tableOne[]" type="checkbox" value="Respiratory Health" />
                                         Respiratory Health
                                     </label>
                                 </td>
@@ -324,7 +339,7 @@ function IsInjected($str)
                                 <td></td>
                                 <td>
                                     <label>
-                                        <input name="ctl22" type="checkbox" value="99" />
+                                        <input name="tableOne[]" type="checkbox" value="Other" />
                                         Other
                                     </label>
                                 </td>
@@ -346,19 +361,19 @@ function IsInjected($str)
                             <tr>
                                 <td>
                                     <label>
-                                        <input name="ctl25" type="checkbox" value="1" />
+                                        <input name="tableTwo[]" type="checkbox" value="Study Design" />
                                         Study Design
                                     </label>
                                 </td>
                                 <td>
                                     <label>
-                                        <input name="ctl27" type="checkbox" value="4" />
+                                        <input name="tableTwo[]" type="checkbox" value="Grant Proposal Development" />
                                         Grant Proposal Development
                                     </label>
                                 </td>
                                 <td>
                                     <label>
-                                        <input name="ctl29" type="checkbox" value="7" />
+                                        <input name="tableTwo[]" type="checkbox" value="Education" />
                                         Education
                                     </label>
                                 </td>
@@ -366,19 +381,19 @@ function IsInjected($str)
                             <tr>
                                 <td>
                                     <label>
-                                        <input name="ctl32" type="checkbox" value="2" />
+                                        <input name="tableTwo[]" type="checkbox" value="Database Design & Management" />
                                         Database Design & Management
                                     </label>
                                 </td>
                                 <td>
                                     <label>
-                                        <input name="ctl34" type="checkbox" value="5" />
+                                        <input name="tableTwo[]" type="checkbox" value="Bioinformatics Analysis" />
                                         Bioinformatics Analysis
                                     </label>
                                 </td>
                                 <td>
                                     <label>
-                                        <input name="ctl36" type="checkbox" value="9" />
+                                        <input name="tableTwo[]" type="checkbox" value="Other" />
                                         Other
                                     </label>
                                 </td>
@@ -386,13 +401,13 @@ function IsInjected($str)
                             <tr>
                                 <td>
                                     <label>
-                                        <input name="ctl39" type="checkbox" value="3" />
+                                        <input name="tableTwo[]" type="checkbox" value="Biostatistical Data Analysis" />
                                         Biostatistical Data Analysis
                                     </label>
                                 </td>
                                 <td>
                                     <label>
-                                        <input name="ctl41" type="checkbox" value="6" />
+                                        <input name="tableTwo[]" type="checkbox" value="Dissemination" />
                                         Dissemination
                                     </label>
                                 </td>
@@ -427,7 +442,7 @@ function IsInjected($str)
                     </div>
                 </div>
                 <br />
-                <div class="row">
+                <!--<div class="row">
                     <div class="col-sm-2">
                         <label class="control-label">Enter Code Below</label>
                     </div>
@@ -436,9 +451,9 @@ function IsInjected($str)
 
                     </div>
                 </div>
-                <br />
+                <br />-->
 
-                <div id="UP1">
+               <!--<div id="UP1">
 
                     <div class="row form-group-md" ">
                         <div class="col-sm-2">
@@ -446,23 +461,50 @@ function IsInjected($str)
                         </div>
                         <div class="col-sm-2">
                             <img src="./html-contact-form-captcha/captcha_code_file.php?rand=<?php echo rand(); ?>" id='captchaimg'>
-                            <!--<img id="imgCaptcha" class="img-responsive" src="GenerateCaptcha?636123875456386485" />-->
+                            <img id="imgCaptcha" class="img-responsive" src="GenerateCaptcha?636123875456386485" />
                         </div>
-                        <div class="col-sm-1">
+                        <!--<div class="col-sm-1">
                             <button onclick="__doPostBack('btnRefresh','')" id="btnRefresh" class="btn btn-info">
                                 <span class="glyphicon glyphicon-refresh"></span> Refresh
                             </button>
                         </div>
                     </div>
 
-                </div>
+                </div>-->
 
                 <br />
-                <div class="row">
-                    <div class="col-sm-12">
+                <div class="row form-group-md">
+                    <!--<div class="col-sm-12">
                         <input type="submit" name="btnSubmit" value="Submit" id="btnSubmit" name="btnSubmit" class="btn center-block" />
+                    </div>-->
+					<div class="col-sm-2">
+                            <label class="control-label">Captcha code</label>
                     </div>
-                </div>
+					<div class="col-sm-2">
+						<img src="./html-contact-form-captcha/captcha_code_file.php?rand=<?php echo rand(); ?>" id='captchaimg' ><br>
+						<label for='message'>Enter the code above here :</label><br>
+					</div>
+					<div class="col-sm-2">
+						<input class="form-control" id="6_letters_code" name="6_letters_code" type="text"><br>
+						<small>Can't read the image? click <a href='javascript: refreshCaptcha();'>here</a> to refresh</small>
+						</p>
+					</div>
+				</div>
+					<div class="row text-center>
+					
+					<div class="col-sm-2">
+					<input type="submit" value="Submit" name='submit' class="btn center-block">
+					</div>
+                
+				<script language='JavaScript' type='text/javascript'>
+					function refreshCaptcha()
+					{
+					var img = document.images['captchaimg'];
+					img.src = img.src.substring(0,img.src.lastIndexOf("?"))+"?rand="+Math.random()*1000;
+					}
+				</script>
+				
+				
 
             
 
@@ -512,33 +554,9 @@ function IsInjected($str)
                 },
                 txtProjectTitle: {
                     required: true
-                },
-                txtCaptchaCode: {
-                    required: true,
-                    remote: function () {
-                        return {
-                            async: false,
-                            url: "ClientRequestForm.aspx/IsCaptchaCodeValid",
-                            type: "POST",
-                            contentType: "application/json; charset=utf-8",
-                            dataType: "json",
-                            data: JSON.stringify({ captchaCode: $('#txtCaptchaCode').val() }),
-                            dataFilter: function (data) {
-                                var msg = JSON.parse(data);
-                                if (msg.hasOwnProperty('d'))
-                                    return msg.d;
-                                else
-                                    return msg;
-                            }
-                        }
-                    }
                 }
-            },
-            messages: {
-                txtCaptchaCode: {
-                    remote: "Invalid Code"
-                }
-            },
+                
+            }/**,
             
             invalidHandler: function (event, validator) {
                 //alert('number of invalid fields: ' + validator.numberOfInvalids());
@@ -554,7 +572,7 @@ function IsInjected($str)
             onfocusout: function (element) {
                 if (element.id != "txtCaptchaCode")
                     $(element).valid();
-            }
+            }*/
         });
 
         
